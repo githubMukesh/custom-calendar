88466e306c03ef0640d942ed08cb1137bce30476
githubMukesh <getmkr@hotmail.com>
2020-09-05 22:41:57 +0530

    Table header for calendar

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+
+function createCalendar(element,month,year){
+    let table = `<table><tr><th>Monday</th><th>Tuesday</th><th>Wednesday</th>
+       <th>Thursday</th><th>Friday</th><th>Saturday</th><th>Sunday</th></tr><tr>`;
+    
+    element.innerHTML += table;   
+}
+
+createCalendar(appDiv,9,2019);
